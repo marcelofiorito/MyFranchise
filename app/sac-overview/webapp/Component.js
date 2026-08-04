811e5581e6fb6bb9527f70397d8687fe8435afa2
Marcelo Fiorito <marcelo.fiorito@sap.com>
2026-08-04 15:17:59 -0300

    fix: iFrame SAC com altura calc(100vh - 44px) para preencher tela completa

--- app/sac-overview/webapp/Component.js
+++ app/sac-overview/webapp/Component.js
@@ -6,6 +6,10 @@ sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
         },
         init: function () {
             UIComponent.prototype.init.apply(this, arguments);
+            // Force full height
+            document.documentElement.style.height = "100%";
+            document.body.style.height = "100%";
+            document.body.style.overflow = "hidden";
         }
     });
 });
